36b2b8a19b1edaf17f66fc8d3edee8f7212341e9
erretres <diegoperezcorreo@gmail.com>
2020-04-16 17:08:14 -0400

    Updates

---
+++ precache-manifest.a14800e13dfab476006fc796e2b1f3d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3597abe6ab7eaa1659f7792b75984f9",
+    "url": "/react-xstate-calc/index.html"
+  },
+  {
+    "revision": "df3a77e4f2b7aad77e5b",
+    "url": "/react-xstate-calc/static/css/main.77cd2061.chunk.css"
+  },
+  {
+    "revision": "55cac901695ef5e09d23",
+    "url": "/react-xstate-calc/static/js/2.19ed3a07.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/react-xstate-calc/static/js/2.19ed3a07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df3a77e4f2b7aad77e5b",
+    "url": "/react-xstate-calc/static/js/main.3da01d15.chunk.js"
+  },
+  {
+    "revision": "a0e0d4a692dfd3117c49",
+    "url": "/react-xstate-calc/static/js/runtime-main.d2946ddf.js"
+  }
+]);
